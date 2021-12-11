7583a66186f7a450ad52340ed2cf6eaf237a6e7b
Yusuke Miyata <yskmyt.dev@gmail.com>
2021-12-12 01:32:37 +0900

    Initial Commit

---
+++ SwiftUI-Banner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Banner
+//
+//  Created by Yusuke Miyata on 2021/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Banner/SwiftUI_BannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BannerApp.swift
+//  SwiftUI-Banner
+//
+//  Created by Yusuke Miyata on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
